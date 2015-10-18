45fa9f6c3c1fea19d3ce3e27a84fef6da32a70a6
atxFarley <afarley@austin.rr.com>
2015-10-18 17:04:34 -0500

    website

---
+++ amyfarley_web/src/main/webapp/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoAmyfarleyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yoAmyfarleyApp
+ */
+angular.module('amyFarleyApp')
+        .controller('AboutCtrl', function () {
+            this.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+        });
---
+++ amyfarley_web/src/main/webapp/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoAmyfarleyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoAmyfarleyApp
+ */
+angular.module('amyFarleyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
